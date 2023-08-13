7e1950d694ef87e26fbd2995239e4a365db9201a
Rupesh Yadav <79595858+Rupesh-132@users.noreply.github.com>
2023-08-13 15:13:47 +0530

    Add files via upload

--- audiocraft/adversarial/discriminators/__init__.py
+++ audiocraft/adversarial/discriminators/__init__.py
@@ -3,20 +3,8 @@
 #
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-"""Adversarial losses and discriminator architectures."""
 
 # flake8: noqa
-from .discriminators import (
-    MultiPeriodDiscriminator,
-    MultiScaleDiscriminator,
-    MultiScaleSTFTDiscriminator
-)
-from .losses import (
-    AdversarialLoss,
-    AdvLossType,
-    get_adv_criterion,
-    get_fake_criterion,
-    get_real_criterion,
-    FeatLossType,
-    FeatureMatchingLoss
-)
+from .mpd import MultiPeriodDiscriminator
+from .msd import MultiScaleDiscriminator
+from .msstftd import MultiScaleSTFTDiscriminator
